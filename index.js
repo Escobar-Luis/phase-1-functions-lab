25ffce912b5a3222e42a46c9853d14efe4262395
Luis Escobar <lae311@stern.nyu.edu>
2021-10-12 16:26:08 -0400

    commit tree 99227d5e78625e5a0170ccc52d94f9c61d623c12

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks (start) {
+    const distance = Math.abs(42 - start);
+    return distance;
+}
+
+function distanceFromHqInFeet (start) {
+    const feet = distanceFromHqInBlocks (start) * 264;
+    return feet;
+}
+
+function distanceTravelledInFeet (start, destination) {
+    const distance = Math.abs(start - destination);
+    const feet = distance * 264;
+    return feet;
+}
+
+function calculatesFarePrice (start, destination) {
+    const feet = distanceTravelledInFeet (start, destination);
+    let price;
+    if (feet<400) {
+        price = 0;}                                             
+    else if (feet >400 && feet < 2000) {
+        price = (feet - 400) * .02;}
+    else if (feet > 2000 && feet < 2500) {
+        price = 25}
+    else if (feet > 2500) {
+        price = "cannot travel that far"
+    }
+    return price;
+}
